cd8c42c1830e0cd55e71f6cb3daf5dcc3fb7945b
zyqdragon <zhangjdr@163.com>
2022-05-15 08:32:24 -0400

    Update export.py

--- one_input_two_outputs/export.py
+++ one_input_two_outputs/export.py
@@ -1,34 +1,26 @@
 import torch
 import os
 import sys
-sys.path.append('../LIAN')
-sys.path.append('./')
+sys.path.append('../upper_directory')
 
-from cvp.models import build_model
-from lian.config import predict_argument_parser, predict_setup
+import new_model
+from func_file import predict_argument_parser, func_setup
 
 def main(args):
     os.environ["EXPORT_ONNX_MODEL"] = "ON"
-
-    cfg = predict_setup(args)
-    predictor = build_model(cfg)
-    weight = torch.load(args.weight_path, map_location="cpu")['model']
-    predictor.load_state_dict(weight)
-    predictor = predictor.eval().cuda()
+    # cft denote configure file
+    cft = func_setup(args) 
+    model_cfg = new_model(cft)
+    weight = torch.load(weight_path, map_location="cpu")['model']
+    model_cfg.load_state_dict(weight)
+    model_cfg = model_cfg.eval().cuda()
     in_tensor = torch.rand(1, 3, 480, 480).cuda()
-    predictor.forward(in_tensor)
+    model_cfg.forward(in_tensor)
 
-    if cfg.dataset.task == "pld":
-        onnx_file = "pld.onnx"
-        torch.onnx.export(predictor, (in_tensor, ), onnx_file,
+    onnx_file = "test.onnx"
+    torch.onnx.export(model_cfg, (in_tensor, ), onnx_file,
                             input_names=['input_0'],
-                            output_names=['output_0', 'output_1'])
-    else:
-        onnx_file = "fs.onnx"
-        torch.onnx.export(predictor, (in_tensor, ), onnx_file,
-                            input_names=['input_0'],
-                            output_names=['output_0'])
-
+                            output_names=['output_0', 'output_1']
 
 if __name__ == "__main__":
     args = predict_argument_parser().parse_args()
